e4d087b0a9326e420244fd51c7ef9df8d8844b0e
AmrAbaza29 <134836859+AmrAbaza29@users.noreply.github.com>
2025-02-17 17:45:17 +0200

    add-reset-and-add-mobile-number-attr

--- custom-authenticator-attribute/src/main/java/com/example/keycloak/authenticator/UserDataDisplayAuthenticator.java
+++ custom-authenticator-attribute/src/main/java/com/example/keycloak/authenticator/UserDataDisplayAuthenticator.java
@@ -6,10 +6,11 @@ import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 
 public class UserDataDisplayAuthenticator extends AbstractFormAuthenticator {
-
 
     @Override
     public void authenticate(AuthenticationFlowContext context) {
@@ -22,18 +23,30 @@ public class UserDataDisplayAuthenticator extends AbstractFormAuthenticator {
         String email = user.getEmail();
         String customAttr1 = user.getFirstAttribute("ContractNumber");
         String customAttr2 = user.getFirstAttribute("RegistrationFormNumber");
+        String mobileNumber = user.getFirstAttribute("MobileNumber");
 
         Response challenge = context.form()
-            .setAttribute("email", email)
-            .setAttribute("customAttr1", customAttr1)
-            .setAttribute("customAttr2", customAttr2)
-            .createForm("user-data-display.ftl");
+                .setAttribute("email", email)
+                .setAttribute("customAttr1", customAttr1)
+                .setAttribute("customAttr2", customAttr2)
+                .setAttribute("mobileNumber", mobileNumber)
+                .createForm("user-data-display.ftl");
 
         context.challenge(challenge);
     }
 
     @Override
     public void action(AuthenticationFlowContext context) {
+        MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
+        String actionParam = formData.getFirst("action");
+
+        if ("cancel".equals(actionParam)) {
+            context.resetFlow();
+            return;
+        }
+        // Marks the authentication process as successful.
+        // This should be called when the authentication action is completed
+        // successfully.
         context.success();
     }
 
